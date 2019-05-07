8d22f9eb7edb63c9984bbb1eb4c0c121bf3eb812
Lyasinkovska <l.yasinkovska1@gmail.com>
2019-05-07 14:23:08 +0300

    added new test to test_cities.py

--- 11/test_cities.py
+++ 11/test_cities.py
@@ -13,7 +13,7 @@ class CityCountryTestCase(unittest.TestCase):
     def test_city_country_population(self):
         """Do location such as "Lviv, Ukraine - 1000000" work? """
         your_location = location_name("lviv", "ukraine", "123")
-        self.assertEqual(your_location, "Lviv, Ukraine - 123")
+        self.assertEqual(your_location, "Lviv, Ukraine - Population 123")
 
 
 if __name__ == '__main__':
