3e576a06975ced4e02ee80c09d5cab8f08debb40
Lyasinkovska <l.yasinkovska1@gmail.com>
2019-05-31 13:25:17 +0300

    Bullet class created in bullet.py

--- alien_invasion_student_version/ship.py
+++ alien_invasion_student_version/ship.py
@@ -16,8 +16,8 @@ class Ship():
         self.rect.center = self.screen_rect.center
         
         # Store a decimal value for the ship's center.
-        self.center = float(self.rect.centerx)
-        self.centery = float(self.rect.centerx)
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
 
         # Movement flag
         self.moving_right = False
@@ -30,18 +30,17 @@ class Ship():
         """ Update the ship's position based on the movement flag."""
         if self.moving_right and self.rect.right < self.screen_rect.right:
             # Update the ship's center value, not the rect
-            self.center += self.ai_settings.ship_speed_factor
+            self.centerx += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
-            self.center -= self.ai_settings.ship_speed_factor
-
-
-        if self.moving_up and self.rect.top < self.rect.top:
+            self.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > 0:
             self.centery -= self.ai_settings.ship_speed_factor
         if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.centery += self.ai_settings.ship_speed_factor
 
         #Update rect object from self.center.
-        self.rect.centerx = self.center
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
 
 
 
